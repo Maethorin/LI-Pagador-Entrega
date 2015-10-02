253d55c1bb38d5435a14bc77d4cf6f53e4e097b8
maethorin <maethorin@gmail.com>
2015-10-02 16:57:57 -0300

    Versionando pagadores #54214877308154

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='li-pagador-entrega',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/lojaintegrada/LI-Pagador-Entrega',
     license='MIT',
     description=u'Meio de pagamento usado na entrega em loja',
